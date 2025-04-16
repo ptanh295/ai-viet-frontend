1fbd6989d20f1cdde4aa6cb11a1729de1ef37d71
ptanh295 <59157654+ptanh295@users.noreply.github.com>
2025-04-16 20:40:08 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,1 @@
-import PromptGenerator from './PromptGenerator';
 
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <PromptGenerator />
-    </div>
-  );
-}
-
-export default App;
